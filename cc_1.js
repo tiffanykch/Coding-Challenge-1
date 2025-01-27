65605f3ce193a26c47bcc30bdf4af3da08e9270a
Tiffany Kuo Chang <tiffanyk104@usf.edu>
2025-01-26 20:51:43 -0500

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,15 @@
+//TASK 1: VARIABLES - RECORDING EMPLOYEE INFORMATION
+
+//Create variable to save employee's name as a string
+let employeeName = "Employee's Name"
+
+//Create immutable variable to save employee's ID
+const employeeID = 16800
+
+//Create boolean to verify employee's employment status
+var isActive = true
+
+//Logging each variable's values and data type to the console
+console.log("Employee Name:", employeeName, "- Data Type:", typeof employeeName)
+console.log("Employee ID:", employeeID, "- Data Type:", typeof employeeID)
+console.log("Employment Status:", isActive, "- Data Type:", typeof isActive)
