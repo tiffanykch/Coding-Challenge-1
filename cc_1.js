ab3ecbaa9c2f216ff393e76b000ab7e2ddf3963d
Tiffany Kuo Chang <tiffanyk104@usf.edu>
2025-01-26 21:02:14 -0500

    Task 2 - Product Details

--- cc_1.js
+++ cc_1.js
@@ -10,6 +10,23 @@ const employeeID = 16800
 var isActive = true
 
 //Logging each variable's values and data type to the console
-console.log("Employee Name:", employeeName, "- Data Type:", typeof employeeName)
-console.log("Employee ID:", employeeID, "- Data Type:", typeof employeeID)
-console.log("Employment Status:", isActive, "- Data Type:", typeof isActive)
+console.log("Employee Name:", employeeName, "- Type:", typeof employeeName)
+console.log("Employee ID:", employeeID, "- Type:", typeof employeeID)
+console.log("Employment Status:", isActive, "- Type:", typeof isActive)
+
+//TASK 2: PRIMITIVE DATA TYPES - MANAGING PRODUCT DETAILS
+
+//Create variable to save product's name as string
+let productName = "Product Name"
+
+//Create immutable variable to record product price
+const productPrice = 8888
+
+//Create boolean to indicate product availability
+var isAvailable = false
+
+//Logging each variable's value and data type to the console
+console.log("Product Name:", productName, "- Type:", typeof productName)
+console.log("Product Price: $" + productPrice, "- Type:", typeof productPrice)
+console.log("Product Availability:", isAvailable, "- Type:", typeof isAvailable)
+
